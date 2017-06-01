d8b673ed110bb95649d2e55f27a0d18fbd6fdf6f
Greg Turnquist <gturnquist@pivotal.io>
2017-06-01 16:16:54 -0500

    Fix ResourceProcessor to handle multiple Resources
    * Bump things up to latest Spring Boot stable release to ensure we're using latest version of Spring Data + Spring HATEOAS
    * Fix CitizensResourcesProcessor to use Resources<Resource<Citizen>> instead of Resources<Citizen>.

--- src/main/java/de/wilms/springtest/processors/CitizenResourcesProcessor.java
+++ src/main/java/de/wilms/springtest/processors/CitizenResourcesProcessor.java
@@ -1,17 +1,23 @@
 package de.wilms.springtest.processors;
 
 import de.wilms.springtest.entities.Citizen;
+
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.hateoas.Resources;
 import org.springframework.stereotype.Component;
 
+/**
+ * Because Spring Data REST wraps things as {@literal Resources<Resource<T>>}, that is the type you must
+ * match when writing a custom {@link ResourceProcessor}. --Greg Turnquist
+ */
 @Component
-public class CitizenResourcesProcessor implements ResourceProcessor<Resources<Citizen>> {
+public class CitizenResourcesProcessor implements ResourceProcessor<Resources<Resource<Citizen>>> {
 
     public static boolean GOT_CALLED = false;
 
     @Override
-    public Resources<Citizen> process(Resources<Citizen> citizens) {
+    public Resources<Resource<Citizen>> process(Resources<Resource<Citizen>> citizens) {
         System.out.println("Hello from the ResourceSSSSProcessor!");
         GOT_CALLED = true;
         return citizens;
